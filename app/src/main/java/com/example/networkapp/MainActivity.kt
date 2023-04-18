115c27aecf9832ca0fc78606934127f7453b9267
Kevina <kevina585@gmail.com>
2023-04-18 12:24:08 -0400

    Activity

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -6,14 +6,16 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +26,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
+
+    private lateinit var file: File
+    private val filename = "json_text"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,18 +43,47 @@ class MainActivity : AppCompatActivity() {
         showButton = findViewById<Button>(R.id.showComicButton)
         comicImageView = findViewById<ImageView>(R.id.comicImageView)
 
+        file = File(filesDir, filename)
+
+        if(file.exists()){
+            try{
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line:String?
+                while(br.readLine().also {line=it} != null){
+                    text.append(line)
+                   text.append("/n")
+                }
+                br.close()
+                showComic(JSONObject(text.toString()))
+            } catch(e : IOException){
+                e.printStackTrace()
+            }
+        }
+
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
+
 
     }
 
     private fun downloadComic (comicId: String) {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
-            JsonObjectRequest(url, {showComic(it)}, {
+            JsonObjectRequest(url, {
+                try{
+                    val outputStream = FileOutputStream(file)
+                    outputStream.write(it.toString().toByteArray())
+                    outputStream.close()
+                } catch (e:Exception) {
+                    e.printStackTrace()
+                }
+                showComic(it)
+                                   }, {
             })
         )
+
     }
 
     private fun showComic (comicObject: JSONObject) {
@@ -56,6 +91,5 @@ class MainActivity : AppCompatActivity() {
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
     }
-
 
 }
